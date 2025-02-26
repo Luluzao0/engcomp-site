efcb0a7f62952f641ec9a7767bf2d211489a365d
luis <40541878+Luluzao0@users.noreply.github.com>
2025-02-26 17:43:46 -0300

    fix: "navbar e footer"

--- js/footer-component.js
+++ js/footer-component.js
@@ -1,41 +1,43 @@
 class FooterComponent extends HTMLElement {
-    connectedCallback() {
-      this.innerHTML = `
-        <!-- Rodapé -->
-        <footer>
-          <div class="container">
-            <div class="row">
-              <!-- Logo UEMA no rodapé -->
-              <div class="col-md-3 mb-3">
-                <img src="assets/engcomp1.png" alt="Logo UEMA" class="logo-rodape" />
-              </div>
-              <!-- Links rápidos -->
-              <div class="col-md-3 mb-3 rodape-links">
-                <h5>Acesso Rápido</h5>
-                <ul class="list-unstyled">
-                  <li><a href="#">Portal do Aluno</a></li>
-                  <li><a href="#">Biblioteca</a></li>
-                  <li><a href="#">Calendário Acadêmico</a></li>
-                  <li><a href="#">Sistema Interno</a></li>
-                </ul>
-              </div>
-              <!-- Informações de contato -->
-              <div class="col-md-3 mb-3">
-                <h5>Contato</h5>
-                <p>Rua Exemplo, 123 - São Luís, MA</p>
-                <p>Telefone: (98) 99999-9999</p>
-                <p>E-mail: <a href="mailto:contato@uema.br">contato@uema.br</a></p>
-              </div>
+  connectedCallback() {
+    // Lê o atributo "base-url". Se não for informado, assume '/' (raiz)
+    const base = this.getAttribute('base-url') || '/';
+    console.log('Footer base definida:', base);
+    this.innerHTML = `
+      <!-- Rodapé -->
+      <footer>
+        <div class="container">
+          <div class="row">
+            <!-- Logo UEMA no rodapé -->
+            <div class="col-md-3 mb-3">
+              <img src="${base}assets/engcomp1.png" alt="Logo UEMA" class="logo-rodape" />
             </div>
-            <div class="rodape-copy">
-              <p>© 2025 UEMA - Todos os direitos reservados.</p>
-              <p>Desenvolvido por <a href="https://www.linkedin.com/in/1lgl/">Luis Guilherme</a></p>
+            <!-- Links rápidos -->
+            <div class="col-md-3 mb-3 rodape-links">
+              <h5>Acesso Rápido</h5>
+              <ul class="list-unstyled">
+                <li><a href="${base}portal-do-aluno.html">Portal do Aluno</a></li>
+                <li><a href="${base}biblioteca.html">Biblioteca</a></li>
+                <li><a href="${base}calendario-academico.html">Calendário Acadêmico</a></li>
+                <li><a href="${base}sistema-interno.html">Sistema Interno</a></li>
+              </ul>
+            </div>
+            <!-- Informações de contato -->
+            <div class="col-md-3 mb-3">
+              <h5>Contato</h5>
+              <p>Rua Exemplo, 123 - São Luís, MA</p>
+              <p>Telefone: (98) 99999-9999</p>
+              <p>E-mail: <a href="mailto:contato@uema.br">contato@uema.br</a></p>
             </div>
           </div>
-        </footer>
-      `;
-    }
+          <div class="rodape-copy">
+            <p>© 2025 UEMA - Todos os direitos reservados.</p>
+            <p>Desenvolvido por <a href="https://www.linkedin.com/in/1lgl/">Luis Guilherme</a> e <a href="https://www.linkedin.com/in/pmelo22/">Patrick Melo</a></p>
+          </div>
+        </div>
+      </footer>
+    `;
   }
-  
-  customElements.define('footer-component', FooterComponent);
-  
+}
+
+customElements.define('footer-component', FooterComponent);
--- js/navbar-component.js
+++ js/navbar-component.js
@@ -1,11 +1,14 @@
 class NavbarComponent extends HTMLElement {
   connectedCallback() {
+    // Lê o atributo "base-url" diretamente
+    const base = this.getAttribute('base-url') || '/';
+    console.log('Base definida:', base); // deve mostrar "../" na página em "curso"
     this.innerHTML = `
       <!-- Navbar principal -->
       <nav class="navbar navbar-expand-lg">
         <div class="container">
-          <a class="navbar-brand d-flex align-items-center" href="index.html">
-            <img src="assets/engecomp.png" alt="Logo UEMA" style="max-height: 40px" class="me-2" />
+          <a class="navbar-brand d-flex align-items-center" href="${base}index.html">
+            <img src="${base}assets/engecomp.png" alt="Logo UEMA" style="max-height: 40px" class="me-2" />
             <span class="text-white fw-bold d-none d-md-inline">Portal UEMA</span>
           </a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,7 +17,7 @@ class NavbarComponent extends HTMLElement {
           <div class="collapse navbar-collapse" id="navbarMenu">
             <ul class="navbar-nav ms-auto">
               <li class="nav-item">
-                <a class="nav-link" href="index.html">Home</a>
+                <a class="nav-link" href="${base}index.html">Home</a>
               </li>
               <!-- Dropdown Curso -->
               <li class="nav-item dropdown">
@@ -22,14 +25,14 @@ class NavbarComponent extends HTMLElement {
                   Curso
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="cursoDropdown">
-                  <li><a class="dropdown-item" href="curso/direcao-secretaria.html">Direção e Secretaria</a></li>
-                  <li><a class="dropdown-item" href="curso/docentes.html">Docentes</a></li>
-                  <li><a class="dropdown-item" href="curso/colegiado.html">Colegiado</a></li>
-                  <li><a class="dropdown-item" href="curso/nucleo-docente-estruturante.html">Núcleo Docente Estruturante</a></li>
-                  <li><a class="dropdown-item" href="curso/infraestrutura.html">Infraestrutura</a></li>
-                  <li><a class="dropdown-item" href="curso/projeto-pedagogico1.html">Projeto Pedagógico</a></li>
-                  <li><a class="dropdown-item" href="curso/grade-curricular.html">Grade Curricular</a></li>
-                  <li><a class="dropdown-item" href="curso/estagio-supervisionado.html">Estágio Supervisionado</a></li>
+                  <li><a class="dropdown-item" href="${base}curso/direcao-secretaria.html">Direção e Secretaria</a></li>
+                  <li><a class="dropdown-item" href="${base}curso/docentes.html">Docentes</a></li>
+                  <li><a class="dropdown-item" href="${base}curso/colegiado.html">Colegiado</a></li>
+                  <li><a class="dropdown-item" href="${base}curso/nucleo-docente-estruturante.html">Núcleo Docente Estruturante</a></li>
+                  <li><a class="dropdown-item" href="${base}curso/infraestrutura.html">Infraestrutura</a></li>
+                  <li><a class="dropdown-item" href="${base}curso/projeto-pedagogico1.html">Projeto Pedagógico</a></li>
+                  <li><a class="dropdown-item" href="${base}curso/grade-curricular.html">Grade Curricular</a></li>
+                  <li><a class="dropdown-item" href="${base}curso/estagio-supervisionado.html">Estágio Supervisionado</a></li>
                 </ul>
               </li>
               <!-- Dropdown Gestão -->
@@ -38,9 +41,9 @@ class NavbarComponent extends HTMLElement {
                   Gestão
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="gestaoDropdown">
-                  <li><a class="dropdown-item" href="gestão/empresa-junior.html">Empresa Júnior</a></li>
-                  <li><a class="dropdown-item" href="gestão/diretoria-academica.html">Diretoria Acadêmica</a></li>
-                  <li><a class="dropdown-item" href="gestão/projetos-especiais.html">Projetos Especiais</a></li>
+                  <li><a class="dropdown-item" href="${base}gestão/empresa-junior.html">Empresa Júnior</a></li>
+                  <li><a class="dropdown-item" href="${base}gestão/diretoria-academica.html">Diretoria Acadêmica</a></li>
+                  <li><a class="dropdown-item" href="${base}gestão/projetos-especiais.html">Projetos Especiais</a></li>
                 </ul>
               </li>
               <!-- Dropdown Pesquisa -->
@@ -49,10 +52,10 @@ class NavbarComponent extends HTMLElement {
                   Pesquisa
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="pesquisaDropdown">
-                  <li><a class="dropdown-item" href="pesquisa/iniciacao-cientifica.html">Iniciação Científica</a></li>
-                  <li><a class="dropdown-item" href="pesquisa/pibic.html">PIBIC</a></li>
-                  <li><a class="dropdown-item" href="pesquisa/pibiti.html">PIBITI</a></li>
-                  <li><a class="dropdown-item" href="pesquisa/laboratorios.html">Laboratórios</a></li>
+                  <li><a class="dropdown-item" href="${base}pesquisa/iniciacao-cientifica.html">Iniciação Científica</a></li>
+                  <li><a class="dropdown-item" href="${base}pesquisa/pibic.html">PIBIC</a></li>
+                  <li><a class="dropdown-item" href="${base}pesquisa/pibiti.html">PIBITI</a></li>
+                  <li><a class="dropdown-item" href="${base}pesquisa/laboratorios.html">Laboratórios</a></li>
                 </ul>
               </li>
               <!-- Dropdown Departamentos -->
@@ -61,9 +64,9 @@ class NavbarComponent extends HTMLElement {
                   Departamentos
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="departamentosDropdown">
-                  <li><a class="dropdown-item" href="departamentos/noticias.html">Notícias</a></li>
-                  <li><a class="dropdown-item" href="departamentos/projetos.html">Projetos</a></li>
-                  <li><a class="dropdown-item" href="departamentos/publicacoes.html">Publicações</a></li>
+                  <li><a class="dropdown-item" href="${base}departamentos/noticias.html">Notícias</a></li>
+                  <li><a class="dropdown-item" href="${base}departamentos/projetos.html">Projetos</a></li>
+                  <li><a class="dropdown-item" href="${base}departamentos/publicacoes.html">Publicações</a></li>
                 </ul>
               </li>
               <!-- Dropdown Indicadores -->
@@ -72,7 +75,7 @@ class NavbarComponent extends HTMLElement {
                   Indicadores
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="indicadoresDropdown">
-                  <li><a class="dropdown-item" href="indicadores/estatisticas.html">Estatísticas</a></li>
+                  <li><a class="dropdown-item" href="${base}indicadores/estatisticas.html">Estatísticas</a></li>
                 </ul>
               </li>
               <!-- Dropdown Contato -->
@@ -81,8 +84,8 @@ class NavbarComponent extends HTMLElement {
                   Contato
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="contatoDropdown">
-                  <li><a class="dropdown-item" href="contato/contato.html">Informações de Contato</a></li>
-                  <li><a class="dropdown-item" href="contato/localizacao.html">Localização</a></li>
+                  <li><a class="dropdown-item" href="${base}contato/contato.html">Informações de Contato</a></li>
+                  <li><a class="dropdown-item" href="${base}contato/localizacao.html">Localização</a></li>
                 </ul>
               </li>
             </ul>
